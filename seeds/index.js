e5414018daff173cd0d629dc9a4876f8a240b9f1
Dmitriy Diktovnyy <diktovnyy.d@gmail.com>
2023-04-22 20:36:33 +0600

    fix seeds

--- seeds/index.js
+++ seeds/index.js
@@ -14,7 +14,7 @@ const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
-await mongoose.connect(dbUrl);
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 
